2f4891c2d0db022d2eaec9ff783d93a244689610
podhmo <ababjam61+github@gmail.com>
2021-10-03 09:41:58 +0900

    fix: add check

--- cmd/reflect-shape-dump/main.go
+++ cmd/reflect-shape-dump/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"text/template"
 )
 
@@ -42,13 +43,28 @@ func run() error {
 `
 
 func main() {
+	log.SetFlags(0)
 	if err := run(); err != nil {
 		log.Fatalf("!! %+v", err)
 	}
 }
 
 func run() error {
+	if len(os.Args) <= 1 {
+		log.Fatalf("please run <cmd> <fullpath>\n\t e.g. go run <> github.com/podhmo/reflect-shape.Struct\n")
+	}
 	t := template.Must(template.New("code").Parse(code))
+	fullpath := os.Args[1]
+	parts := strings.Split(fullpath, ".")
+	path := strings.Join(parts[:len(parts)-1], ".")
+	name := parts[len(parts)-1]
+
+	log.Printf("check by go list.\tpath=%q\tname=%q", path, name)
+	ctx := context.Background()
+	if err := exec.CommandContext(ctx, "go", "list", path).Run(); err != nil {
+		return err
+	}
+
 	d, err := ioutil.TempDir(".", ".reflect-shape")
 	if err != nil {
 		return err
@@ -68,14 +84,14 @@ func run() error {
 
 	log.Println("create", filepath.Join(d, "main.go"))
 	if err := t.Execute(f, map[string]string{
-		"Path":      "github.com/podhmo/reflect-shape",
+		"Path":      path,
 		"Qualifier": "x",
-		"Name":      "Function",
+		"Name":      name,
 	}); err != nil {
 		return err
 	}
 
-	cmd := exec.CommandContext(context.Background(), "go", "run", filepath.Join(d, "main.go"))
+	cmd := exec.CommandContext(ctx, "go", "run", filepath.Join(d, "main.go"))
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
