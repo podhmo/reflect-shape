0ec8c06b394c218e68f5a638fa89fc310269e9da
podhmo <ababjam61+github@gmail.com>
2022-08-21 22:08:25 +0900

    feat: lookup from struct (prototype)

--- metadata/lookup.go
+++ metadata/lookup.go
@@ -2,15 +2,22 @@ package metadata
 
 import (
 	"fmt"
+	"go/ast"
 	"go/parser"
 	"go/token"
+	"log"
 	"reflect"
 	"runtime"
+	"runtime/debug"
 	"strings"
 
 	"github.com/podhmo/commentof"
 	"github.com/podhmo/commentof/collect"
+	"golang.org/x/tools/go/packages"
 )
+
+// ErrNotFound is the error metadata is not found.
+var ErrNotFound = fmt.Errorf("not found")
 
 type Lookup struct {
 	Fset *token.FileSet
@@ -84,4 +91,59 @@ func (l *Lookup) LookupFromFunc(fn interface{}) (*Func, error) {
 		return nil, fmt.Errorf("function not found,")
 	}
 	return &Func{pc: pc, Raw: result}, nil
+}
+
+type Struct struct {
+	Raw *collect.Object
+}
+
+func (l *Lookup) LookupFromStruct(ob interface{}) (*Struct, error) {
+	rt := reflect.TypeOf(ob)
+	obname := rt.Name()
+	pkgpath := rt.PkgPath()
+	if pkgpath == "main" {
+		binfo, ok := debug.ReadBuildInfo()
+		if !ok {
+			log.Println("debug.ReadBuildInfo() is failed")
+			return nil, ErrNotFound
+		}
+		pkgpath = binfo.Path
+	}
+
+	cfg := &packages.Config{
+		Fset: l.Fset,
+		Mode: packages.NeedName | packages.NeedFiles | packages.NeedSyntax,
+	}
+	pkgs, err := packages.Load(cfg, pkgpath)
+	if err != nil {
+		return nil, fmt.Errorf("packages.Load() %w", err)
+	}
+
+	for _, pkg := range pkgs {
+		if len(pkg.Errors) > 0 {
+			for _, err := range pkg.Errors {
+				log.Printf("pkg %s error: %+v", pkg, err)
+			}
+			continue
+		}
+		if pkg.ID != pkgpath {
+			continue
+		}
+		tree := &ast.Package{Name: pkg.Name, Files: map[string]*ast.File{}}
+		for _, f := range pkg.Syntax {
+			filename := l.Fset.File(f.Pos()).Name()
+			tree.Files[filename] = f
+		}
+
+		p, err := commentof.Package(l.Fset, tree)
+		if err != nil {
+			return nil, fmt.Errorf("collect: dir=%s, name=%s, %w", pkg.PkgPath, obname, err)
+		}
+		result, ok := p.Structs[rt.Name()]
+		if !ok {
+			continue
+		}
+		return &Struct{Raw: result}, nil
+	}
+	return nil, ErrNotFound
 }
