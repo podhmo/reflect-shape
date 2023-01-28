d4ff86c183c19b24d05d1dde523ee8ec4c2cfb5a
podhmo <ababjam61+github@gmail.com>
2023-01-29 00:57:41 +0900

    refactor: unexport extractor, but Visited() method only export refs #56

--- api.go
+++ api.go
@@ -14,7 +14,7 @@ type Config struct {
 
 	DocTruncationSize int
 
-	Extractor *Extractor
+	extractor *Extractor
 	lookup    *metadata.Lookup
 }
 
@@ -32,13 +32,17 @@ func (c *Config) Extract(ob interface{}) *Shape {
 		c.lookup.IncludeGoTestFiles = c.IncludeGoTestFiles
 		c.lookup.IncludeUnexported = true
 	}
-	if c.Extractor == nil {
-		c.Extractor = &Extractor{
+	if c.extractor == nil {
+		c.extractor = &Extractor{
 			Config:   c,
 			Lookup:   c.lookup,
 			seen:     map[ID]*Shape{},
 			packages: map[string]*Package{},
 		}
 	}
-	return c.Extractor.Extract(ob)
+	return c.extractor.Extract(ob)
+}
+
+func (c *Config) Visited() map[ID]*Shape {
+	return c.extractor.Visited()
 }
